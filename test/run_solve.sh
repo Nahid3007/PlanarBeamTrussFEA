6cdc95fc4ffa90071ef55b70058b3845b31cd82d
Nahid Miah <n.miah@web.de>
2022-12-22 14:53:54 +0100

    re-run both models due to change in boundary conditions

--- test/run_solve.sh
+++ test/run_solve.sh
@@ -5,13 +5,13 @@
 input_path='./input_files/rod_truss_test/'
 
 filename=(
-"rod_fixed_displacement"
-"rod_fixed_point_load"
+# "rod_fixed_displacement"
+# "rod_fixed_point_load"
 "plane_truss_triangle_F"
 "plane_truss_triangle_U"
-"plane_bridge_structure_LC_1"
-"plane_bridge_structure_LC_2"
-"truss_structure_w_six_members"
+# "plane_bridge_structure_LC_1"
+# "plane_bridge_structure_LC_2"
+# "truss_structure_w_six_members"
 # "ws_11_12"
 # "beam_w_square_cross_section_LC_1"
 # "beam_w_square_cross_section_LC_2"
